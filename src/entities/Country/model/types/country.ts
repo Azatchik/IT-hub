a840ab78d25b0f54959a04453146f532776b0ad8
Azatchik <131662492+Azatchik@users.noreply.github.com>
2024-07-24 12:56:21 +0500

    Add files via upload

---
+++ src/entities/Country/model/types/country.ts
@@ -0,0 +1,9 @@
+export enum Country {
+    Russia = 'Russia',
+    Belarus = 'Belarus',
+    Ukraine = 'Ukraine',
+    Kazakhstan = 'Kazahstan',
+    Armenia = 'Armenia',
+    Uzbekistan = 'Uzbekistan',
+    USA = 'USA',
+}
